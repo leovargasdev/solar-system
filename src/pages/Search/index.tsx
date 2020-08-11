23034cf3d99c1dc90f8a28844e357c32f423f5ad
Leo Vargas <leu1607@hotmail.com>
2020-08-10 22:39:23 -0300

    Criando o layout da tela de busca

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,12 +1,66 @@
 import React from 'react';
+import { ScrollView, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-import { Container, Title } from './styles';
+import Layout from '../../components/Layout/';
+import { Text } from '../../components/Layout/styles';
+import InputSearch from '../../components/InputSearch/';
+
+import { Header, Box, IconPlanet, BoxContent, Title, Button} from './styles';
+
+import Neptune from '../../assets/planets/neptune.svg';
+import Mars from '../../assets/planets/mars.svg';
 
 const Search: React.FC = () => {
   return (
-    <Container>
-      <Title>Tab Buscar</Title>
-    </Container>
+    <Layout>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 20}}>
+        <Header>
+          <Feather color="#FFF" size={26} name="arrow-left"/>
+          <Feather color="#FFF" size={26} name="settings"/>
+        </Header>
+
+        <Text type="title" style={{marginTop: 20}}>Resultados da busca</Text>
+      
+        <InputSearch />
+
+        <Box>
+          <IconPlanet>
+            <Neptune width={225} height={225}/>
+          </IconPlanet>
+          <BoxContent>
+            <Title>
+              <Text type="title" style={{flex: 1}}>Netuno</Text>
+              <Feather name="bookmark" size={26} color="#FFF"/>
+            </Title>
+            <Text type="small" opacity>Netuno é o oitavo planeta do Sistema Solar, o último a partir do Sol desde a reclassificação...</Text>
+            <Button onPress={() => {}}>
+              <Text type="small" bold>Continue lendo</Text>
+              <Feather name="arrow-right" size={18} color="#EF5F53" style={{marginLeft: 8}}/>
+            </Button>
+          </BoxContent>
+        </Box>
+
+        <Text type="normal" style={{marginTop: 30}}>Você também pode gostar</Text>
+
+        <Box>
+          <IconPlanet>
+            <Mars width={225} height={225}/>
+          </IconPlanet>
+          <BoxContent>
+            <Title>
+              <Text type="title" style={{flex: 1}}>Marte</Text>
+              <Feather name="bookmark" size={26} color="#FFF"/>
+            </Title>
+            <Text type="small" opacity>Marte é o quarto planeta a partir do Sol, o segundo menor do Sistema Solar. Batizado em homena...</Text>
+            <Button onPress={() => {}}>
+              <Text type="small" bold>Continue lendo</Text>
+              <Feather name="arrow-right" size={18} color="#EF5F53" style={{marginLeft: 8}}/>
+            </Button>
+          </BoxContent>
+        </Box>
+      </ScrollView>
+    </Layout>
   );
 }
 
--- src/pages/Search/styles.ts
+++ src/pages/Search/styles.ts
@@ -1,13 +1,41 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
+export const Header = styled.View`
+  flex-direction: row;
+  margin-top: 48px;
+  justify-content: space-between;
 `;
 
-export const Title = styled.Text`
-  font-size: 20px;
-  font-weight: 700;
-  color: #FFF;
+export const Box = styled.View`
+  margin-top: 30px;
+  flex: 1;
+  height: 200px;
+  flex-direction: row;
+  border-radius: 8px;
+  position: relative;
+  background: ${({theme}) => theme.colors.black};
+  overflow: hidden;
+`;
+
+export const IconPlanet = styled.View`
+  position: absolute;
+  left: -24%;
+  top: -25%;
+`;
+
+export const BoxContent = styled.View`
+  padding: 25px 10px;
+  justify-content: space-between;
+  margin-left: 145px;
+`;
+
+export const Title = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Button = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
 `;
